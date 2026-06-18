7ffe3680a2a343a97ca2812fe936072701e0871d
Nnamdi Ogbonna <mannnie55@gmail.com>
2026-06-18 18:58:34 +0100

    chore(sanity): update sanity.cli.ts dataset and projectId fallback configuration

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,8 +2,8 @@ import { defineCliConfig } from "sanity/cli";
 
 export default defineCliConfig({
   api: {
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? "ruboxzxa",
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ?? "production",
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   },
-  studioHost: 'nnamdiogbonna'
+  studioHost: 'nnamdiogbonna',
 });
